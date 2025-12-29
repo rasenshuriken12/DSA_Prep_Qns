f4d05f9424bdcec72ac1795a884dbb7c9021e86e
🪐 StellarCloud12 <auxiliaryds@gmail.com>
2025-12-29 09:07:52 +0530

    Update src.java

--- LeetCode/Arrays/Easy/1_Two_Sum/src.java
+++ LeetCode/Arrays/Easy/1_Two_Sum/src.java
@@ -3,25 +3,19 @@ import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Create a hash map to store number & index
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();  // Create a hash map to store number & index
         
         // Iterate through the array
         for (int i = 0; i < nums.length; i++) {
-            // Calculate what number we need to reach target
-            int complement = target - nums[i];
+            int complement = target - nums[i];        // Calculate what number we need to reach target
             
-            // Check if we've already seen this needed number
+            // Check if the needed number is already in our Hash map
             if (map.containsKey(complement)) {
-                // Found the pair! Return indices
-                return new int[] {map.get(complement), i};
+                return new int[] {map.get(complement), i};     // Found the pair! Return indices
             }
-            
-            // Store current number with its index for future reference
-            map.put(nums[i], i);
+            map.put(nums[i], i);     // Store current number with its index for future reference
         }
         
-        // According to problem, we should never reach here
         return new int[0];
     }
 }
